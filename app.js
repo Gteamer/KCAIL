4ef804139b215eca68fd3834f065597cfea57621
Timm <tarricktimm@gmail.com>
2018-08-23 22:38:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-var express = require('express');
-var app = express();
+//Require HTTPS module
+const https = require('https');
+//Require HTTP module
+const http = require('http');
+//Require the JSON text
+const api = require('./api.json');
 
-// view engine setup
-// app.set('view engine', 'pug');
-// app.set('views', __dirname + '/views');
+//{"total_population": {"date": "2018-08-14", "population": 329110601}}
+function printPopulation(population) {
+    let totalPop = population.total_population.population.toLocaleString();
+    const message = `The current population in ${totalPop}`;
+    console.log(message);
+}
 
-// include routes
-// var routes = require('./routes/index');
-// app.use('/', routes);
+function get(query) {
+    const request = http.get(`http://api.population.io:80/1.0/population/${api.id}/${api.date}/`, response => {
+        let body = '';
+        //Read the data
+        response.on('data', chunk => {
+            body += chunk;
+        });
+        response.on('end', () => {
+            const population = JSON.parse(body);
+            //Print out the data
+            printPopulation(population);
+            // console.log(body);
+        });
+    });
+};
 
-// GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
-});
-
-app.listen(3001, function(), {
-  console.log('Express app listening on port 3000');
-});
+module.exports.get = get;
